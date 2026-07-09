d64be32d25decc0c71b411672b3b19da6fdbc9c3
pnburanapur-png <pnburanapur@gmail.com>
2026-07-09 22:45:18 +0530

    Update first.py

--- first.py
+++ first.py
@@ -3,3 +3,9 @@ if(num>10):
     print(num,"greter than")
 elif(num<10):
     print(num,"Smaller than")
+
+#write a progarm to print avg of two number
+num = int(input("Enter the number:"))
+num1 = int(input("Enter the second number:"))
+Avg = (num+num1 / 2)
+print("Avg of two number is:",Avg)
